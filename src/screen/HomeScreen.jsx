3c2435034a7704f95d5f9c583a6be24204a36a2b
nafis200 <nafisahamed14@gmail.com>
2024-09-20 00:10:38 +0600

    height

--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -1,14 +1,28 @@
 
 
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,TextInput } from 'react-native'
 import React from 'react'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import LinearGradient from 'react-native-linear-gradient';
 import Header from '../components/Header';
+import Fontisto from 'react-native-vector-icons/Fontisto'
 const HomeScreen = () => {
   return (
     <LinearGradient colors={['#FDF0F3','#FFFBFC']} style={styles.conatiner}>
       <Header/>
+      <Text style={styles.matchText}>Match Your Style</Text>
+
+      <View style={styles.inputcontainer}>
+           <View style={styles.iconContainer}>
+           <Fontisto name={"search"} size={26} color={"#C0C0C0"}
+           />
+           </View>
+           <TextInput style={styles.textinput}
+           placeholder="Search"
+           placeholderTextColor="#C0C0C0"
+           />
+       </View>
+
     </LinearGradient>
   )
 }
@@ -19,5 +33,27 @@ const styles = StyleSheet.create({
     conatiner:{
         flex: 1,
         padding:20
-    }
+    },
+    matchText:{
+        fontSize:28,
+        color:'black',
+        marginTop:20
+      },
+      textinput:{
+        flex: 1,
+        color:'black'
+    },
+    inputcontainer:{
+        backgroundColor:'#FFFFFF',
+        height:48,
+        borderRadius:12,
+        alignItems:'center',
+        flexDirection:'row',
+        marginVertical:20
+      },
+      iconContainer:{
+        marginHorizontal:20,
+     }
+ 
+   
 })
