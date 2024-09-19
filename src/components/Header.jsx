5d51ccc7d7ff8043d8835cb26ad8b26c3c43ceaf
nafis200 <nafisahamed14@gmail.com>
2024-09-19 23:57:23 +0600

    hellow typescript

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+
+
+import { StyleSheet, Text, View,Image } from 'react-native'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <View>
+       <View>
+           <Image source={require('../assets/apps.png')} style={styles.appIconContainer} />
+       </View>
+       <Image source={require("../assets/Ellipse2.png")} style={styles.appIcon} />
+
+    </View>
+  )
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+    appIconContainer:{
+        backgroundColor:'#FFFFFF',
+        height:44,
+        width:44,
+        borderRadius:22,
+        justifyContent:'center',
+        alignItems:'center',
+     },
+     appIcon:{
+        height:40,
+        width:40
+    },
+
+
+})
--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -3,20 +3,20 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Icon from 'react-native-vector-icons/FontAwesome';
-
+import LinearGradient from 'react-native-linear-gradient';
+import Header from '../components/Header';
 const HomeScreen = () => {
   return (
-    <View>
-    <Icon.Button
-    name="facebook"
-    backgroundColor="#3b5998"
-  >
-    Login with Facebook
-  </Icon.Button>
-    </View>
+    <LinearGradient colors={['#FDF0F3','#FFFBFC']} style={styles.conatiner}>
+      <Header/>
+    </LinearGradient>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    conatiner:{
+        flex: 1,
+    }
+})
