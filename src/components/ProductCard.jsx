26e0b81bbff7df099261f45554886da8860b0adb
nafis200 <nafisahamed14@gmail.com>
2024-09-20 12:48:16 +0600

    1t season done

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,21 @@
 
-import { StyleSheet, Text, View,Image } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View,Image,TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 
 const ProductCard = () => {
+  const [isLiked,setIsLiked] = useState(false)
   return (
     <View style={styles.container}>
       <Image style={styles.coverImage} source={require('../assets/girl1.png')} />
       <Text style={styles.title}>Jacket Jeans</Text>
       <Text style={styles.price}>$119.0</Text>
     
-      <View style={styles.likeContainer}>
-      <AntDesign name={"hearto"} size={20} color={"red"} />
-      </View>
+      <TouchableOpacity onPress={()=>setIsLiked(!isLiked)} style={styles.likeContainer}>
+      {
+         isLiked? <AntDesign name={"heart"} size={20} color={"red"} /> : <AntDesign name={"hearto"} size={20} color={"red"} />
+      }
+      </TouchableOpacity>
 
     </View>
   )
